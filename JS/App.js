c8ff647b45c3676ee6913c461a3524ee70cba11b
Armandocm19 <78453450+Armandocm19@users.noreply.github.com>
2021-03-02 16:15:45 -0600

    Add files via upload

---
+++ JS/App.js
@@ -0,0 +1,398 @@
+window.sr = ScrollReveal();
+
+sr.reveal('Body', {
+   duration: 3000
+});
+
+sr.reveal('.contenedor-galeria', {
+    duration: 5000,
+    origin: 'top',
+    distance: '-100px'
+ });
+
+ sr.reveal('.contenedor-sobre', {
+    duration: 5000,
+    origin: 'top',
+    distance: '-100px'
+ });
+
+let modal = document.getElementById('modal');
+let flex = document.getElementById('flex');
+let abrir = document.getElementById('uno');
+let cerrar = document.getElementById('close');
+
+abrir.addEventListener('click', function(e){
+    e.preventDefault();
+    modal.style.display = 'block';
+});
+
+
+cerrar.addEventListener('click', function(){
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+    if(e.target == flex){
+        modal.style.display = 'none'
+    }
+});
+
+let modal2 = document.getElementById('modal2');
+let flex2 = document.getElementById('flex2');
+let abrir2 = document.getElementById('dos');
+let cerrar2 = document.getElementById('close2');
+
+abrir2.addEventListener('click', function(e){
+   e.preventDefault();
+   modal2.style.display = 'block';
+});
+
+
+cerrar2.addEventListener('click', function(){
+   modal2.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex2){
+       modal2.style.display = 'none'
+   }
+});
+
+let modal3 = document.getElementById('modal3');
+let flex3 = document.getElementById('flex3');
+let abrir3 = document.getElementById('tres');
+let cerrar3 = document.getElementById('close3');
+
+abrir3.addEventListener('click', function(e){
+   e.preventDefault();
+   modal3.style.display = 'block';
+});
+
+
+cerrar3.addEventListener('click', function(){
+   modal3.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex3){
+       modal3.style.display = 'none'
+   }
+});
+
+let modal4 = document.getElementById('modal4');
+let flex4 = document.getElementById('flex4');
+let abrir4 = document.getElementById('cuatro');
+let cerrar4 = document.getElementById('close4');
+
+abrir4.addEventListener('click', function(e){
+   e.preventDefault();
+   modal4.style.display = 'block';
+});
+
+
+cerrar4.addEventListener('click', function(){
+   modal4.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex4){
+       modal4.style.display = 'none'
+   }
+});
+
+let modal5 = document.getElementById('modal5');
+let flex5 = document.getElementById('flex5');
+let abrir5 = document.getElementById('cinco');
+let cerrar5 = document.getElementById('close5');
+
+abrir5.addEventListener('click', function(e){
+   e.preventDefault();
+   modal5.style.display = 'block';
+});
+
+
+cerrar5.addEventListener('click', function(){
+   modal5.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex5){
+       modal5.style.display = 'none'
+   }
+});
+
+
+let modal6 = document.getElementById('modal6');
+let flex6 = document.getElementById('flex6');
+let abrir6 = document.getElementById('seis');
+let cerrar6 = document.getElementById('close6');
+
+abrir6.addEventListener('click', function(e){
+   e.preventDefault();
+   modal6.style.display = 'block';
+});
+
+
+cerrar6.addEventListener('click', function(){
+   modal6.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex6){
+       modal6.style.display = 'none'
+   }
+});
+
+
+let modal7 = document.getElementById('modal7');
+let flex7 = document.getElementById('flex7');
+let abrir7 = document.getElementById('siete');
+let cerrar7 = document.getElementById('close7');
+
+abrir7.addEventListener('click', function(e){
+   e.preventDefault();
+   modal7.style.display = 'block';
+});
+
+
+cerrar7.addEventListener('click', function(){
+   modal7.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex7){
+       modal7.style.display = 'none'
+   }
+});
+
+let modal8 = document.getElementById('modal8');
+let flex8 = document.getElementById('flex8');
+let abrir8 = document.getElementById('ocho');
+let cerrar8 = document.getElementById('close8');
+
+abrir8.addEventListener('click', function(e){
+   e.preventDefault();
+   modal8.style.display = 'block';
+});
+
+
+cerrar8.addEventListener('click', function(){
+   modal8.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex8){
+       modal8.style.display = 'none'
+   }
+});
+
+
+let modal9 = document.getElementById('modal9');
+let flex9 = document.getElementById('flex9');
+let abrir9 = document.getElementById('nueve');
+let cerrar9 = document.getElementById('close9');
+
+abrir9.addEventListener('click', function(e){
+   e.preventDefault();
+   modal9.style.display = 'block';
+});
+
+
+cerrar9.addEventListener('click', function(){
+   modal9.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex9){
+       modal9.style.display = 'none'
+   }
+});
+
+let modal10 = document.getElementById('modal10');
+let flex10 = document.getElementById('flex10');
+let abrir10 = document.getElementById('diez');
+let cerrar10 = document.getElementById('close10');
+
+abrir10.addEventListener('click', function(e){
+   e.preventDefault();
+   modal10.style.display = 'block';
+});
+
+
+cerrar10.addEventListener('click', function(){
+   modal10.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex10){
+       modal10.style.display = 'none'
+   }
+});
+
+let modal11 = document.getElementById('modal11');
+let flex11 = document.getElementById('flex11');
+let abrir11 = document.getElementById('once');
+let cerrar11 = document.getElementById('close11');
+
+abrir11.addEventListener('click', function(e){
+   e.preventDefault();
+   modal11.style.display = 'block';
+});
+
+
+cerrar11.addEventListener('click', function(){
+   modal11.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex11){
+       modal11.style.display = 'none'
+   }
+});
+
+let modal12 = document.getElementById('modal12');
+let flex12 = document.getElementById('flex12');
+let abrir12 = document.getElementById('doce');
+let cerrar12 = document.getElementById('close12');
+
+abrir12.addEventListener('click', function(e){
+   e.preventDefault();
+   modal12.style.display = 'block';
+});
+
+
+cerrar12.addEventListener('click', function(){
+   modal12.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex12){
+       modal12.style.display = 'none'
+   }
+});
+
+let modal13 = document.getElementById('modal13');
+let flex13 = document.getElementById('flex13');
+let abrir13 = document.getElementById('trece');
+let cerrar13 = document.getElementById('close13');
+
+abrir13.addEventListener('click', function(e){
+   e.preventDefault();
+   modal13.style.display = 'block';
+});
+
+
+cerrar13.addEventListener('click', function(){
+   modal13.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex13){
+       modal13.style.display = 'none'
+   }
+});
+
+let modal14 = document.getElementById('modal14');
+let flex14 = document.getElementById('flex14');
+let abrir14 = document.getElementById('catorce');
+let cerrar14 = document.getElementById('close14');
+
+abrir14.addEventListener('click', function(e){
+   e.preventDefault();
+   modal14.style.display = 'block';
+});
+
+
+cerrar14.addEventListener('click', function(){
+   modal14.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex14){
+       modal14.style.display = 'none'
+   }
+});
+
+let modal15 = document.getElementById('modal15');
+let flex15 = document.getElementById('flex15');
+let abrir15 = document.getElementById('quince');
+let cerrar15 = document.getElementById('close15');
+
+abrir15.addEventListener('click', function(e){
+   e.preventDefault();
+   modal15.style.display = 'block';
+});
+
+
+cerrar15.addEventListener('click', function(){
+   modal15.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex15){
+       modal15.style.display = 'none'
+   }
+});
+
+let modal16 = document.getElementById('modal16');
+let flex16 = document.getElementById('flex16');
+let abrir16 = document.getElementById('dieciseis');
+let cerrar16 = document.getElementById('close16');
+
+abrir16.addEventListener('click', function(e){
+   e.preventDefault();
+   modal16.style.display = 'block';
+});
+
+
+cerrar16.addEventListener('click', function(){
+   modal16.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex16){
+       modal16.style.display = 'none'
+   }
+});
+
+let modal17 = document.getElementById('modal17');
+let flex17 = document.getElementById('flex17');
+let abrir17 = document.getElementById('diecisiete');
+let cerrar17 = document.getElementById('close17');
+
+abrir17.addEventListener('click', function(e){
+   e.preventDefault();
+   modal17.style.display = 'block';
+});
+
+
+cerrar17.addEventListener('click', function(){
+   modal17.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex17){
+       modal17.style.display = 'none'
+   }
+});
+
+let modal18 = document.getElementById('modal18');
+let flex18 = document.getElementById('flex18');
+let abrir18 = document.getElementById('dieciocho');
+let cerrar18 = document.getElementById('close18');
+
+abrir18.addEventListener('click', function(e){
+   e.preventDefault();
+   modal18.style.display = 'block';
+});
+
+
+cerrar18.addEventListener('click', function(){
+   modal18.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+   if(e.target == flex18){
+       modal18.style.display = 'none'
+   }
+});
